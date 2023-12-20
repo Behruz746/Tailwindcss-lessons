70774ee1c925658beb50b19e1ea6aa99c44f75b0
Behruz <madaminovbehruz746@gmail.com>
2023-12-20 15:08:25 +0500

    added: first commit

---
+++ lesson-tailwindcss/src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="w-full h-screen flex items-center justify-center bg-[#4271aa]">
+        <a
+          href="https://tailwindcss.com/"
+          target="_blank"
+          className="text-[#000] text-[45px] font-[500] font-sans hover:font-mono cursor-pointer"
+          rel="noreferrer"
+        >
+          Hello my first project with tailwindcss
+        </a>
+      </div>
+    </>
+  );
+}
+
+export default App;
